cb479950dc052ef8799a666ffeb13f7a127a3060
Akshay Mude <akshaymude2021@gmail.com>
2022-09-05 06:42:12 +0530

    commit

---
+++ src/First/Day.java
@@ -0,0 +1,20 @@
+package First;
+
+public class Day {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1, num2, addition, mul;
+        num1 = 50;
+		num2 = 20;
+		addition = num1+num2;
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(addition);
+		
+		System.out.println("hbfubfdfiuvifudvnifdnvifdnv");
+		System.out.println("1234567947616856554566");
+	}
+
+}
